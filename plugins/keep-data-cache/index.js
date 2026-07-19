27934f0249af779ea61ca55813d12932146a04f9
Aaron Gustafson <aaron@easy-designs.net>
2026-07-19 07:57:02 -0700

    Going to try temporarily disabling the cache read, so we don’t pick it up

--- plugins/keep-data-cache/index.js
+++ plugins/keep-data-cache/index.js
@@ -4,8 +4,8 @@ module.exports = {
   //  - the file/directory already exists locally
   //  - the file/directory has not been cached yet
   async onPreBuild({ utils }) {
-    await utils.cache.restore('./_data/results-last-runs.json');
-    await utils.cache.restore('./_data/results');
+    //await utils.cache.restore('./_data/results-last-runs.json');
+    //await utils.cache.restore('./_data/results');
   },
   // Cache file/directory for future builds.
   // Does not do anything if:
