8f2a96cc8c86714594f7f14000da4b6927385069
Aaron Gustafson <aaron@easy-designs.net>
2026-07-19 08:12:23 -0700

    Switching to a new set of data storage files to bust the cache in Netlify

--- _data/lastruns.js
+++ _data/lastruns.js
@@ -1,10 +1,10 @@
-// Nunjucks workaround for dashes in global `results-last-runs.json` variable name
+// Nunjucks workaround for dashes in global `results-last-runs-new.json` variable name
 
 let lastruns = {};
 
 try {
-	lastruns = require("./results-last-runs.json");
-} catch(e) {
+  lastruns = require("./results-last-runs-new.json");
+} catch (e) {
 }
 
 module.exports = lastruns;
---
+++ _data/results.js
@@ -0,0 +1,26 @@
+const fastglob = require("fast-glob");
+
+module.exports = async function() {
+	let results = {};
+	
+	let resultFiles = await fastglob("./_data/results-new/**/*.json", {
+		caseSensitiveMatch: false
+	});
+
+	for (let resultFile of resultFiles) {
+		let split = resultFile.split("/");
+		let filename = split.pop();
+		let hash = split.pop();
+
+		if (!results[hash]) {
+			results[hash] = {};
+		}
+
+		// Use the filename (without .json) as the key
+		let key = filename.replace(/\.json$/, "");
+		let resultData = require(`.${resultFile}`);
+		results[hash][key] = resultData;
+	}
+
+	return results;
+};
