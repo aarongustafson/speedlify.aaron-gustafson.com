5f569cd6c8e79d3569a059cf65ea43d392bd8355
Aaron Gustafson <aaron@easy-designs.net>
2026-07-19 08:12:38 -0700

    Switching to a new set of data storage files to bust the cache in Netlify

--- _data/results.js
+++ _data/results.js
@@ -1,26 +1,26 @@
 const fastglob = require("fast-glob");
 
-module.exports = async function() {
-	let results = {};
-	
-	let resultFiles = await fastglob("./_data/results-new/**/*.json", {
-		caseSensitiveMatch: false
-	});
+module.exports = async function () {
+  let results = {};
 
-	for (let resultFile of resultFiles) {
-		let split = resultFile.split("/");
-		let filename = split.pop();
-		let hash = split.pop();
+  let resultFiles = await fastglob("./_data/results-new/**/*.json", {
+    caseSensitiveMatch: false
+  });
 
-		if (!results[hash]) {
-			results[hash] = {};
-		}
+  for (let resultFile of resultFiles) {
+    let split = resultFile.split("/");
+    let filename = split.pop();
+    let hash = split.pop();
 
-		// Use the filename (without .json) as the key
-		let key = filename.replace(/\.json$/, "");
-		let resultData = require(`.${resultFile}`);
-		results[hash][key] = resultData;
-	}
+    if (!results[hash]) {
+      results[hash] = {};
+    }
 
-	return results;
+    // Use the filename (without .json) as the key
+    let key = filename.replace(/\.json$/, "");
+    let resultData = require(`.${resultFile}`);
+    results[hash][key] = resultData;
+  }
+
+  return results;
 };
